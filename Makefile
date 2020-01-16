aa3671d4579fdb7db120fac2a6cc0ffeceb74a8e
Ethan Frey <ethanfrey@users.noreply.github.com>
2020-01-16 17:51:14 +0100

    Cleanup more

--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
-.PHONY: build view
+.PHONY: build view all
+
+all: build public
 
 build:
 	tinygo build -o main.wasm -target wasm ./main.go
@@ -6,3 +8,6 @@ build:
 view:
 	wasm-nm main.wasm
 
+public:
+	wasm-nm -e main.wasm
+	wasm-nm -i main.wasm
--- main.go
+++ main.go
@@ -8,8 +8,26 @@ func add(a, b int) int {
 	return a + b
 }
 
-//go:export multiply
-func multiply(a, b int) int {
-	return a * b
+// why is this not exported???
+//go:export relic
+func relic(a int) int {
+	l := counter("foobar")
+	return 10 * a + l
 }
 
+// Name mismatch cause no wasm export
+//go:export think
+func foobar(a, b int) int {
+	return a + b
+}
+
+// []byte cause no wasm export
+//go:export bcounter
+func bcounter(a []byte) int {
+	return len(a)
+}
+
+//go:export counter
+func counter(a string) int {
+	return len(a)
+}
